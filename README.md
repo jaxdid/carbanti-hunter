fe1e6d6b1c517201662b7caee73d6d61dd26742c
Alain Lemaire <jaxdid@gmail.com>
2018-05-01 22:14:26 +0100

    Initial commit - bare necessities

---
+++ README.md
@@ -0,0 +1,6 @@
+# Carbanti Hunter
+Find out exactly how much Mk 3 Carbanti Sensor Array Salvage you don't have to upgrade your favorite Star Wars: Galaxy of Heroes characters.
+
+- Preact
+- Flow
+- Webpack + Babel
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
+}

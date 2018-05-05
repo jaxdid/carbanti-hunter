35cc7ac6c308575d943882e269405d0da7b47ccf
Alain Lemaire <jaxdid@gmail.com>
2018-05-05 23:48:48 +0100

    Adds gear requirements by level table to character cards

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function CharacterCard (props) {
   return (
-    <div>
+    <Fragment>
       <div>{props.name}</div>
       <div>
         <h5>Total salvage needed:</h5>
         <h1>{Object.values(props.map).reduce((total, current) => total + current)}</h1>
       </div>
-    </div>
+      <table>
+        {renderGearTable(props.map)}
+      </table>
+    </Fragment>
   )
+}
+
+function renderGearTable (map) {
+  return Object.entries(map).map(([level, gearNeeded]) => {
+    return (
+      <tr>
+        <td>{level}</td>
+        <td>{gearNeeded}</td>
+      </tr>
+    )
+  })
 }
 
 export default CharacterCard
